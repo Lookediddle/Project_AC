af6dd1e5c0a4391083523627306a01834543e789
Lookediddle <luca.giussani2@studenti.unimi.it>
2026-02-13 16:32:43 +0100

    granger aggregation generalized and plots

--- code/main_granger.py
+++ code/main_granger.py
@@ -56,60 +56,101 @@ results = load_data("results/20260203_170946_4_lags_gran_allsubs_no_lags_aggrega
 
 #%% plot ECNs for each group
 ch_names = results[1]["AD"]["pvals"][0].columns # remind indexes' names = columns' names
+n_ch = len(ch_names)
 pos = {"CN":0,"FTD":1,"AD":2}
-min_thresh=0.5 # at least 1 pval across lags 
-max_pval = 1e-12 # if 0: only certain causal links are considered
+thresh_pct=90 # strengths threshold (percentile) 
+max_pval = alpha # if 0: only certain causal links are considered
+n_lags = len(results) # lags count
 
-# create strengths: accumulate accepted pvals across lags
+# create strengths: avg accepted pvals across lags and subjects
 strengths_groups = {
-    "AD":  {"strength": np.zeros((len(ch_names), len(ch_names)))},
-    "FTD": {"strength": np.zeros((len(ch_names), len(ch_names)))},
-    "CN":  {"strength": np.zeros((len(ch_names), len(ch_names)))}}
+    "AD":  {"strength": np.zeros((n_ch, n_ch))},
+    "FTD": {"strength": np.zeros((n_ch, n_ch))},
+    "CN":  {"strength": np.zeros((n_ch, n_ch))}}
 
-for lag,all_groups in results.items():
-    for group,all_ecns in all_groups.items():
-        # keep highest causal links (i.e. the most certain ones -> p-value<=max_pval for the entire group!)
-        pvals_group_mean_curr_lag = np.mean(all_ecns["pvals"], axis=0)
-        strength_group_curr_lag = (np.round(pvals_group_mean_curr_lag,4) <= max_pval).astype(int) # binary (1->link; 0->no link)
-        # accumulate strengths for current lag 
-        strengths_groups[group]["strength"] += strength_group_curr_lag
+# lags aggregation
+for group in strengths_groups.keys():
+    n_subs = len(results[1][group]["pvals"])
+    group_strength_sum = np.zeros((n_ch, n_ch)) # empirical strength: for mean group strength
 
-# find min and max for min-max norm [0,1] for pretty plots
-max_s, min_s = 0, math.inf
+    for subj in range(n_subs):
+        # mean on lags 
+        subj_strength_sum = np.zeros((n_ch, n_ch))
+        
+        for lag in results.keys():  # 1,...,maxlag
+            pvals = results[lag][group]["pvals"][subj]
+
+            # keep only links w/ higher confidence
+            p_masked = (pvals <= max_pval).astype(int) # 1->accepted, 0->not accepted
+
+            subj_strength_sum += p_masked
+        
+        # mean strength for this subject
+        subj_strength_mean = subj_strength_sum / n_lags
+
+        group_strength_sum += subj_strength_mean
+
+    # mean for group
+    strengths_groups[group]["strength"] = group_strength_sum / n_subs
+
+# for pretty plots
+all_strengths, all_n_probs = [], []
+#***max_n_pr, min_n_pr = 0, math.inf # min and max for min-max norm [0,1]
 for group, ecn in strengths_groups.items(): 
-    s = np.abs(ecn["strength"])
+    s = ecn["strength"].to_numpy()
+    all_strengths.append(s.ravel()) # 2D->1D
 
-    #if np.min(s) < min_s: min_s = np.min(s)
-    if np.max(s) > max_s: max_s = np.max(s)
+    #***if np.min(s) < min_n_pr: min_n_pr = np.min(s)
+    #***if np.max(s) > max_n_pr: max_n_pr = np.max(s)
+all_strengths = np.concatenate(all_strengths)
+min_thresh = np.percentile(all_strengths, thresh_pct) # global threshold for strengths!
+
 
 # plot results for each group
-fig, axes = plt.subplots(1, 3, figsize=(13, 6), constrained_layout=True)
-for group, ecn in strengths_groups.items(): 
-    s = ecn["strength"]
-    s_norm = (s-min_thresh) / (max_s-min_thresh) # min-max norm [0,1]
+fig, axes = plt.subplots(1, 3, figsize=(13, 6), constrained_layout=True) 
+for group, ecn in strengths_groups.items():
+        s = ecn["strength"].to_numpy() # already in [0,1]
+        
+        #***s_norm = (s-min_thresh) / (max_n_pr-min_thresh) # min-max norm [0,1]
 
-    strength_group_df = pd.DataFrame(s, index=ch_names, columns=ch_names)
-    norm_df = pd.DataFrame(s_norm, index=ch_names, columns=ch_names)
+        strength_group_df = pd.DataFrame(s, index=ch_names, columns=ch_names)
+        #***s_norm_group_df = pd.DataFrame(s_norm, index=ch_names, columns=ch_names)
+        
+        plot_ecn(strength_group_df, min_thresh, ax=axes[pos[group]], title=group, widths=strength_group_df)
 
-    plot_ecn(strength_group_df, min_thresh, ax=axes[pos[group]], title=group, widths=norm_df)
-fig.suptitle(f"Granger, avg groups (pv_group={max_pval}, th=min_pv_ok_count={min_thresh})", fontsize=16)
+fig.suptitle(f"Granger (pval_max={max_pval}, th_pct={thresh_pct})", fontsize=16)
 #plt.tight_layout() # useless if constrained_layout=True
 plt.show()
 
 save_results() # save figures
 
+# for lag,all_groups in results.items():
+#     for group,all_ecns in all_groups.items():
+#         # keep highest causal links (i.e. the most certain ones -> p-value<=max_pval for the entire group!)
+#         pvals_group_mean_curr_lag = np.mean(all_ecns["pvals"], axis=0)
+#         strength_group_curr_lag = (np.round(pvals_group_mean_curr_lag,4) <= max_pval).astype(int) # binary (1->link; 0->no link)
+#         # accumulate strengths for current lag 
+#         strengths_groups[group]["strength"] += strength_group_curr_lag
+
+# # find min and max for min-max norm [0,1] for pretty plots
+# max_s, min_s = 0, math.inf
+# for group, ecn in strengths_groups.items(): 
+#     s = np.abs(ecn["strength"])
+
+#     #if np.min(s) < min_s: min_s = np.min(s)
+#     if np.max(s) > max_s: max_s = np.max(s)
+
+# # plot results for each group
 # fig, axes = plt.subplots(1, 3, figsize=(13, 6), constrained_layout=True)
-# for group, all_ecns in results.items():
-    
-#     # keep highest causal links (i.e. the certain ones -> p-value=0 for the entire group!)
-#     pvals_group_mean = np.mean(results[group]["pvals"], axis=0)
-#     strength_group = (np.round(pvals_group_mean,4) == 0).astype(int) 
-        
-#     strength_group_df = pd.DataFrame(
-#             strength_group, index=ch_names, columns=ch_names)
-    
-#     plot_ecn(strength_group_df, thresh, ax=axes[pos[group]], title=group)
-# fig.suptitle(f"Granger (highest vals, th={thresh})", fontsize=16)
+# for group, ecn in strengths_groups.items(): 
+#     s = ecn["strength"]
+#     s_norm = (s-min_thresh) / (max_s-min_thresh) # min-max norm [0,1]
+
+#     strength_group_df = pd.DataFrame(s, index=ch_names, columns=ch_names)
+#     norm_df = pd.DataFrame(s_norm, index=ch_names, columns=ch_names)
+
+#     plot_ecn(strength_group_df, min_thresh, ax=axes[pos[group]], title=group, widths=norm_df)
+# fig.suptitle(f"Granger, avg groups (pv_group={max_pval}, th=min_pv_ok_count={min_thresh})", fontsize=16)
 # #plt.tight_layout() # useless if constrained_layout=True
 # plt.show()
 
--- code/main_lingam.py
+++ code/main_lingam.py
@@ -27,35 +27,35 @@ subs_to_groups = {num:label for r,label in ranges for num in r} # e.g. {1:"AD", 
 all_subs_report = load_data("results/20260128_110832_allsubs_stationarity/data/saved_data.pkl")
 
 #%% process ECN
-# results = {}
-# for lag in range(maxlag+1): # 0,...,maxlag
-#     results[lag] = {
-#         "AD":  {"strengths": [], "probs": []},
-#         "FTD": {"strengths": [], "probs": []},
-#         "CN":  {"strengths": [], "probs": []}}
+results = {}
+for lag in range(maxlag+1): # 0,...,maxlag
+    results[lag] = {
+        "AD":  {"strengths": [], "probs": []},
+        "FTD": {"strengths": [], "probs": []},
+        "CN":  {"strengths": [], "probs": []}}
 
-# for subj_dir in subjects:
-#     subj_id = subj_dir.name # i.e. 'sub-xxx'
-#     subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. int('xxx')
-#     print(f"\n- {subj_id} -", end=' ', flush=True)
+for subj_dir in subjects:
+    subj_id = subj_dir.name # i.e. 'sub-xxx'
+    subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. int('xxx')
+    print(f"\n- {subj_id} -", end=' ', flush=True)
 
-#     #%% preprocessing: load and segment
-#     filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
-#     eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
-#     epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
+    #%% preprocessing: load and segment
+    filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
+    eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
+    epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
         
-#     #%% lingam
-#     sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
-#     # lingam+bootstrap
-#     ling_strength, ling_probs = lingam_ecn_boot(epochs, channels, maxlag, current_subject=sub)
-#     #***ling_strength = lingam_ecn(epochs, channels, maxlag, current_subject=sub)
+    #%% lingam
+    sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
+    # lingam+bootstrap
+    ling_strength, ling_probs = lingam_ecn_boot(epochs, channels, maxlag, current_subject=sub)
+    #***ling_strength = lingam_ecn(epochs, channels, maxlag, current_subject=sub)
 
-#     for lag in range(maxlag+1): # 0,...,maxlag
-#         results[lag][subj_group]["strengths"].append(ling_strength[lag])
-#         results[lag][subj_group]["probs"].append(ling_probs[lag])
+    for lag in range(maxlag+1): # 0,...,maxlag
+        results[lag][subj_group]["strengths"].append(ling_strength[lag])
+        results[lag][subj_group]["probs"].append(ling_probs[lag])
 
-# save_results(results)
-results = load_data("results/20260204_201717_4_lags_ling-bootstrap_allsubs_no_lags_aggregation/data/saved_data.pkl")
+save_results(results)
+#results = load_data("results/20260204_201717_4_lags_ling-bootstrap_allsubs_no_lags_aggregation/data/saved_data.pkl")
 
 #%% plot ECNs for each group
 ch_names = results[0]["AD"]["strengths"][0].columns # remind indexes' names = columns' names
@@ -86,7 +86,7 @@ for group in res_groups.keys():
             s = results[lag][group]["strengths"][subj]
             p = results[lag][group]["probs"][subj]
 
-            mask = (p >= min_prob) # keep only strengths w higher probabilities
+            mask = (p >= min_prob) # keep only strengths w/ higher probabilities
             s_masked = np.where(mask, s, 0.0)
 
             subj_strength_sum += s_masked
