0e5da97355e66bdc180204af2da3d4113fdf4165
Lookediddle <luca.giussani2@studenti.unimi.it>
2026-02-13 12:14:40 +0100

    lingam plot as lab paper with bootstrap min_causal_efx=0.2

--- code/functions/lingam.py
+++ code/functions/lingam.py
@@ -7,150 +7,6 @@ from .granger import make_stationary
 from statsmodels.tsa.api import VAR
 from scipy.stats import chi2 # for Wald test on VAR coefs
 
-# time series
-def lingam_ecn(epochs, channels, maxlag=4, current_subject=None, verbose=True):
-    """
-    Compute VAR-LiNGAM (time-series) ECN for one subject by aggregating across epochs.
-
-    Parameters
-    ----------
-    epochs : ndarray, shape (n_epochs, n_channels, n_samples)
-    channels : dict, channel numbers mapped to names (e.g. {0:"Fp1", 1:"Fp2", etc.})
-    maxlag : int
-    alpha : float
-        Confidence level used to check independence (i.e. LiNGAM assumption)
-    current_subject : dict, if not None, it means that the stationarity was already checked 
-        during preprocessing. So stationarity check is skipped when not necessary.
-        Default at None.
-
-    Returns
-    -------
-    mean_strength : dict
-        maps each lag to its pd.DataFrame of causal strengths (n_channels, n_channels)
-    """
-
-    ch_names = [name for _, name in channels.items()]
-    n_channels = len(ch_names)
-    strength_df = {} # {lag:strength_matrix}, e.g.: {0:B0, ..., maxlag:Bmaxlag}
-    all_pvals_df = {} # lags to pvals, test on VAR coefficients
-    for k in range(maxlag+1):
-        strength_df[k] = [] # init
-        if k != 0: # not istantaneous! only for VAR coefficients (i.e. lags)
-            all_pvals_df[k] = [] 
-
-    for e, epoch in enumerate(epochs):
-        if verbose: print(f"... [epoch {e}]", end='-->', flush=True)
-
-        epoch_df = pd.DataFrame(epoch.T, columns=ch_names)
-
-        # 1. make epoch stationary (for VAR model!)
-        if verbose: print('... stationariety', end=' ', flush=True)
-        if current_subject == None:
-            epoch_df, n_diffs, _ = make_stationary(epoch_df, verbose) 
-        
-        else: # stationarity check was done in pre-processing
-            curr_epoch_report = current_subject['epochs'][e]
-            if curr_epoch_report['n_diffs'] > 0: # not stationary!!! differencing was applied in this epoch
-                epoch_df, n_diffs, _ = make_stationary(epoch_df, verbose) 
-
-        # 2. apply VAR-LiNGAM
-        if verbose: print("... ***fitting VAR-LiNGAM***", end=', ', flush=True)
-        model = VARLiNGAM(lags=maxlag, criterion=None) # criterion=None: do not search best lags
-        model.fit(epoch_df)
-
-        B_mats = model.adjacency_matrices_ # adjacency_matrices_: list [B0, B1, ..., Bp]
-
-        # if verbose: print("... independence pvalues ...", end=', ', flush=True)
-        # pvals_independence = model.get_error_independence_p_values() #****solo su B0 istantanea
-
-        for k in range(len(B_mats)): # k=0,...,maxlag
-            strength = pd.DataFrame(B_mats[k], index=ch_names, columns=ch_names)
-            strength_df[k].append(strength)
-
-        # 3. confidence: test on VAR coefficients (lagged effects only)
-        if verbose: print('...test VAR params...', end=' ', flush=True)
-        var_model = VAR(epoch_df)
-        var_res = var_model.fit(maxlag, trend='n') # == VAR in VAR-LiNGAM
-
-        for k in range(1, maxlag+1): # not 0, only lagged
-            pvals = pd.DataFrame(
-                np.ones((n_channels, n_channels)),
-                index=ch_names,
-                columns=ch_names
-            )
-
-            for ch_to in ch_names:
-                for ch_frm in ch_names:
-                    if ch_to == ch_frm: 
-                        continue
-
-                    _, pvals.loc[ch_to, ch_frm] = wald_test_var_coef(var_res, ch_to, ch_frm, k)
-
-            all_pvals_df[k].append(pvals)
-
-
-        # pvals = var_res.pvalues # p-values for model coefficients from Student t-distribution
-        # for lag in range(1, maxlag+1):
-        #     pval_df = pd.DataFrame( # init
-        #         np.ones((len(ch_names), len(ch_names))),
-        #         index=ch_names,
-        #         columns=ch_names
-        #     )
-        #     for to in ch_names:
-        #         for frm in ch_names:
-        #             if to == frm:
-        #                 continue
-
-        #             param = f"L{lag}.{frm}"
-        #             if param in pvals.index:
-        #                 pval_df.loc[to, frm] = pvals.loc[param, to]
-            
-        #     all_pvals_df[lag].append(pval_df)
-
-
-    # aggregate across epochs (i.e. mean), for each lag
-    mean_strength = {}
-    for lag,res in strength_df.items():
-        mean_strength[lag] = sum(res) / len(res)
-    
-    mean_pvals = {}
-    for lag,res in all_pvals_df.items():
-        mean_pvals[lag] = sum(res) / len(res)
-
-    return mean_strength, mean_pvals
-
-
-# Wald test on VAR coefficients
-def wald_test_var_coef(var_res, ch_to, ch_frm, k):
-    """
-    Wald test for H0: A_k(ch_to,ch_frm) = 0 (i.e. no causal strength),
-    where A_k(ch_to,ch_frm) is a VAR coefficient at lag k.
-
-    Returns
-    -------
-    W : float
-        Wald statistic
-    p_value : float
-    """
-
-    param = f"L{k}.{ch_frm}"
-    if param not in var_res.params.index:
-        raise ValueError(f"Coefficient label {param} doesn't exist.")
-
-    # coefficient estimate
-    theta_hat = var_res.params.loc[param, ch_to]
-
-    # variance estimate
-    cov = var_res.cov_params()
-    var_theta = cov.loc[(param, ch_to), (param, ch_to)]
-
-    # Wald
-    W = (theta_hat ** 2) / var_theta
-    p_value = 1 - chi2.cdf(W, df=1)
-
-    return W, p_value
-
-
 # varlingam + bootstrap
 def lingam_ecn_boot(epochs, channels, maxlag=4, current_subject=None, verbose=True):
     """
@@ -206,11 +62,11 @@ def lingam_ecn_boot(epochs, channels, maxlag=4, current_subject=None, verbose=Tr
         # adjacency_matrices_: list [B0, B1, ..., Bp]
         B_mats = model.adjacency_matrices_.copy()
 
-        # reliability: bootstrapping - (***@jit, my optimization: from https://github.com/cdt15/lingam/issues/130 ***)
+        # 3. reliability: bootstrap (***@jit, my optimization: from https://github.com/cdt15/lingam/issues/130 ***)
         print("... bootstrapping", end=', ', flush=True)
         boot_res = model.bootstrap_optimized(epoch_df, n_sampling=10, fit_done=True)
         print("... get probabilities", end=', ', flush=True)
-        probs = boot_res.get_probabilities(min_causal_effect=1e-12) #***min_causal_effect=0.2) # as in VARLiNGAM example
+        probs = boot_res.get_probabilities(min_causal_effect=0.2) # as in VARLiNGAM example on git
 
         for k in range(maxlag+1): # k=0,...,maxlag
             B_df = pd.DataFrame(B_mats[k], index=ch_names, columns=ch_names)
@@ -219,7 +75,7 @@ def lingam_ecn_boot(epochs, channels, maxlag=4, current_subject=None, verbose=Tr
             probs_df = pd.DataFrame(probs[k], index=ch_names, columns=ch_names)
             all_probs_df[k].append(probs_df)
 
-    # aggregate across epochs (i.e. mean), for each lag
+    # 4. aggregate across epochs (i.e. mean), for each lag
     mean_strengths = {}
     for lag,res in all_strengths_df.items():
         mean_strengths[lag] = sum(res) / len(res)
@@ -230,83 +86,155 @@ def lingam_ecn_boot(epochs, channels, maxlag=4, current_subject=None, verbose=Tr
 
     return mean_strengths, mean_probs
 
-# jackknife for reliability -> leave-one-epoch-out
-def lingam_ecn_jk(epochs, channels, maxlag=4, current_subject=None, verbose=False):
+#%% reliability experiments: jackknife and Wald test
+# # ---------- jackknife ---------- 
+# # -> leave-one-epoch-out
+# def lingam_ecn_jk(epochs, channels, maxlag=4, current_subject=None, verbose=False):
     
-    ch_names = [name for _, name in channels.items()]
+#     ch_names = [name for _, name in channels.items()]
 
-    # jackknife
-    N = len(epochs)
-    jk_strengths = []
+#     # jackknife
+#     N = len(epochs)
+#     jk_strengths = []
 
-    print('\n... jackknife w/o epoch:', end=' ', flush=True)
-    for i in range(N):
-        print(f'{i}', end=',', flush=True)
-        epochs_jk = np.delete(epochs, i, axis=0)
-        mean_jk = lingam_ecn(
-            epochs_jk, channels, maxlag, current_subject, verbose
-        )
-        jk_strengths.append(mean_jk.values)
+#     print('\n... jackknife w/o epoch:', end=' ', flush=True)
+#     for i in range(N):
+#         print(f'{i}', end=',', flush=True)
+#         epochs_jk = np.delete(epochs, i, axis=0)
+#         mean_jk = lingam_ecn(
+#             epochs_jk, channels, maxlag, current_subject, verbose
+#         )
+#         jk_strengths.append(mean_jk.values)
     
-    jk_strengths = np.array(jk_strengths)
+#     jk_strengths = np.array(jk_strengths)
 
-    mean_jk = np.mean(jk_strengths, axis=0)
+#     mean_jk = np.mean(jk_strengths, axis=0)
 
-    # jackknife variance: <variance => >link stability
-    var_jk = ((N-1)/N) * np.sum((jk_strengths - mean_jk)**2, axis=0)
+#     # jackknife variance: <variance => >link stability
+#     var_jk = ((N-1)/N) * np.sum((jk_strengths - mean_jk)**2, axis=0)
 
-    std_jk = np.sqrt(var_jk)
+#     std_jk = np.sqrt(var_jk)
 
-    return std_jk
+#     return std_jk
 
-
-# istantaneous, pure lingam******
-# from lingam import ICALiNGAM, DirectLiNGAM
-# def lingam_ecn_no_lags(epochs, channels, threshold=0.01):
+# # varlingam
+# def lingam_ecn(epochs, channels, maxlag=4, current_subject=None, verbose=True):
 #     """
-#     Compute LiNGAM (istantaneous) ECN for one subject by aggregating across epochs.
+#     Compute VAR-LiNGAM (time-series) ECN for one subject by aggregating across epochs.
 
 #     Parameters
 #     ----------
 #     epochs : ndarray, shape (n_epochs, n_channels, n_samples)
-#     channels : dict, channel numbers mapped to names
-#     threshold : float
-#         Threshold on absolute causal strength
+#     channels : dict, channel numbers mapped to names (e.g. {0:"Fp1", 1:"Fp2", etc.})
+#     maxlag : int
+#     alpha : float
+#         Confidence level used to check independence (i.e. LiNGAM assumption)
+#     current_subject : dict, if not None, it means that the stationarity was already checked 
+#         during preprocessing. So stationarity check is skipped when not necessary.
+#         Default at None.
 
 #     Returns
 #     -------
-#     mean_strength : pd.DataFrame (n_channels x n_channels)
-#         Mean absolute causal strengths
-#     binary_adj : pd.DataFrame (n_channels x n_channels)
-#         Binary ECN adjacency
+#     mean_strength : dict
+#         maps each lag to its pd.DataFrame of causal strengths (n_channels, n_channels)
 #     """
 
 #     ch_names = [name for _, name in channels.items()]
-#     all_B = []
+#     n_channels = len(ch_names)
+#     strength_df = {} # {lag:strength_matrix}, e.g.: {0:B0, ..., maxlag:Bmaxlag}
+#     all_pvals_df = {} # lags to pvals, test on VAR coefficients
+#     for k in range(maxlag+1):
+#         strength_df[k] = [] # init
+#         if k != 0: # not istantaneous! only for VAR coefficients (i.e. lags)
+#             all_pvals_df[k] = [] 
 
 #     for e, epoch in enumerate(epochs):
-#         print(f"... [epoch {e}]", end='-->', flush=True)
+#         if verbose: print(f"... [epoch {e}]", end='-->', flush=True)
 
-#         # LiNGAM expects shape (n_samples, n_channels)
 #         epoch_df = pd.DataFrame(epoch.T, columns=ch_names)
 
-#         print("... ***fitting LiNGAM*** ...", end=', ', flush=True)
-#         model = DirectLiNGAM() # DirectLiNGAM converges to right solution (!= ICALiNGAM to local optima!)
+#         # 1. make epoch stationary (for VAR model!)
+#         if verbose: print('... stationariety', end=' ', flush=True)
+#         if current_subject == None:
+#             epoch_df, n_diffs, _ = make_stationary(epoch_df, verbose) 
+        
+#         else: # stationarity check was done in pre-processing
+#             curr_epoch_report = current_subject['epochs'][e]
+#             if curr_epoch_report['n_diffs'] > 0: # not stationary!!! differencing was applied in this epoch
+#                 epoch_df, n_diffs, _ = make_stationary(epoch_df, verbose) 
+
+#         # 2. apply VAR-LiNGAM
+#         if verbose: print("... ***fitting VAR-LiNGAM***", end=', ', flush=True)
+#         model = VARLiNGAM(lags=maxlag, criterion=None) # criterion=None: do not search best lags
 #         model.fit(epoch_df)
 
-#         # adjacency matrix (B_ij = i → j)
-#         B = pd.DataFrame(
-#             model.adjacency_matrix_,
-#             index=ch_names,
-#             columns=ch_names
-#         )
+#         B_mats = model.adjacency_matrices_ # adjacency_matrices_: list [B0, B1, ..., Bp]
 
-#         all_B.append(abs(B))
+#         # if verbose: print("... independence pvalues ...", end=', ', flush=True)
+#         # pvals_independence = model.get_error_independence_p_values() #****solo su B0 istantanea
 
-#     # aggregate across epochs (i.e. mean)
-#     mean_strength = sum(all_B) / len(all_B)
+#         for k in range(len(B_mats)): # k=0,...,maxlag
+#             strength = pd.DataFrame(B_mats[k], index=ch_names, columns=ch_names)
+#             strength_df[k].append(strength)
 
-#     # ECN adjacency (binary) ***secondo me non serve, piuttosto i pvals (se calcolabili)***
-#     binary_adj = (mean_strength > threshold).astype(int)
+#         # 3. confidence: test on VAR coefficients (lagged effects only)
+#         if verbose: print('...test VAR params...', end=' ', flush=True)
+#         var_model = VAR(epoch_df)
+#         var_res = var_model.fit(maxlag, trend='n') # == VAR in VAR-LiNGAM
 
-#     return mean_strength, binary_adj
+#         for k in range(1, maxlag+1): # not 0, only lagged
+#             pvals = pd.DataFrame(
+#                 np.ones((n_channels, n_channels)),
+#                 index=ch_names,
+#                 columns=ch_names
+#             )
+
+#             for ch_to in ch_names:
+#                 for ch_frm in ch_names:
+#                     if ch_to == ch_frm: 
+#                         continue
+
+#                     _, pvals.loc[ch_to, ch_frm] = wald_test_var_coef(var_res, ch_to, ch_frm, k)
+
+#             all_pvals_df[k].append(pvals)
+
+#     # aggregate across epochs (i.e. mean), for each lag
+#     mean_strength = {}
+#     for lag,res in strength_df.items():
+#         mean_strength[lag] = sum(res) / len(res)
+    
+#     mean_pvals = {}
+#     for lag,res in all_pvals_df.items():
+#         mean_pvals[lag] = sum(res) / len(res)
+
+#     return mean_strength, mean_pvals
+
+# # ---------- Wald test on VAR coefficients ----------
+# def wald_test_var_coef(var_res, ch_to, ch_frm, k):
+#     """
+#     Wald test for H0: A_k(ch_to,ch_frm) = 0 (i.e. no causal strength),
+#     where A_k(ch_to,ch_frm) is a VAR coefficient at lag k.
+
+#     Returns
+#     -------
+#     W : float
+#         Wald statistic
+#     p_value : float
+#     """
+
+#     param = f"L{k}.{ch_frm}"
+#     if param not in var_res.params.index:
+#         raise ValueError(f"Coefficient label {param} doesn't exist.")
+
+#     # coefficient estimate
+#     theta_hat = var_res.params.loc[param, ch_to]
+
+#     # variance estimate
+#     cov = var_res.cov_params()
+#     var_theta = cov.loc[(param, ch_to), (param, ch_to)]
+
+#     # Wald
+#     W = (theta_hat ** 2) / var_theta
+#     p_value = 1 - chi2.cdf(W, df=1)
+
+#     return W, p_value
--- code/main_lingam.py
+++ code/main_lingam.py
@@ -27,35 +27,35 @@ subs_to_groups = {num:label for r,label in ranges for num in r} # e.g. {1:"AD", 
 all_subs_report = load_data("results/20260128_110832_allsubs_stationarity/data/saved_data.pkl")
 
 #%% process ECN
-results = {}
-for lag in range(maxlag+1): # 0,...,maxlag
-    results[lag] = {
-        "AD":  {"strengths": [], "probs": []},
-        "FTD": {"strengths": [], "probs": []},
-        "CN":  {"strengths": [], "probs": []}}
+# results = {}
+# for lag in range(maxlag+1): # 0,...,maxlag
+#     results[lag] = {
+#         "AD":  {"strengths": [], "probs": []},
+#         "FTD": {"strengths": [], "probs": []},
+#         "CN":  {"strengths": [], "probs": []}}
 
-for subj_dir in subjects:
-    subj_id = subj_dir.name # i.e. 'sub-xxx'
-    subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. int('xxx')
-    print(f"\n- {subj_id} -", end=' ', flush=True)
+# for subj_dir in subjects:
+#     subj_id = subj_dir.name # i.e. 'sub-xxx'
+#     subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. int('xxx')
+#     print(f"\n- {subj_id} -", end=' ', flush=True)
 
-    #%% preprocessing: load and segment
-    filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
-    eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
-    epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
+#     #%% preprocessing: load and segment
+#     filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
+#     eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
+#     epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
         
-    #%% lingam
-    sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
-    # lingam+bootstrap
-    ling_strength, ling_probs = lingam_ecn_boot(epochs, channels, maxlag, current_subject=sub)
-    #***ling_strength = lingam_ecn(epochs, channels, maxlag, current_subject=sub)
+#     #%% lingam
+#     sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
+#     # lingam+bootstrap
+#     ling_strength, ling_probs = lingam_ecn_boot(epochs, channels, maxlag, current_subject=sub)
+#     #***ling_strength = lingam_ecn(epochs, channels, maxlag, current_subject=sub)
 
-    for lag in range(maxlag+1): # 0,...,maxlag
-        results[lag][subj_group]["strengths"].append(ling_strength[lag])
-        results[lag][subj_group]["probs"].append(ling_probs[lag])
+#     for lag in range(maxlag+1): # 0,...,maxlag
+#         results[lag][subj_group]["strengths"].append(ling_strength[lag])
+#         results[lag][subj_group]["probs"].append(ling_probs[lag])
 
-save_results(results)
-#results = load_data("results/20260205_214516_4_lags_ling-bootstrap_allsubs_no_lags_aggregation_minefx0/data/saved_data.pkl")
+# save_results(results)
+results = load_data("results/20260204_201717_4_lags_ling-bootstrap_allsubs_no_lags_aggregation/data/saved_data.pkl")
 
 #%% plot ECNs for each group
 ch_names = results[0]["AD"]["strengths"][0].columns # remind indexes' names = columns' names
@@ -63,9 +63,9 @@ n_ch = len(ch_names)
 pos = {"CN":0,"FTD":1,"AD":2}
 thresh_pct=90 # strengths threshold (percentile)
 min_prob = 0.95 # ~alpha=0.05
-n_tau = len(results) # istantaneous+lags count
+n_lags = len(results) # istantaneous+lags count
 
-# aggragate strengths and probs: keep max strengths between accepted (i.e. probable) ones across lags
+# aggragate strengths and probs: avg across lags and subjects for each group
 res_groups = {
     "AD":  {"strength": np.zeros((n_ch, n_ch)), "n_probs": np.zeros((n_ch, n_ch))},
     "FTD": {"strength": np.zeros((n_ch, n_ch)), "n_probs": np.zeros((n_ch, n_ch))},
@@ -74,105 +74,64 @@ res_groups = {
 # lags aggregation
 for group in res_groups.keys():
     n_subs = len(results[0][group]["strengths"])
-    group_strength_max = np.full((n_ch, n_ch), 0.0)
-    group_strength_sum = np.zeros((n_ch, n_ch)) # mean across subjects for group
+    group_strength_sum = np.zeros((n_ch, n_ch)) # for mean group strength
+    group_n_probs_sum = np.zeros((n_ch, n_ch)) # for mean group n_probs
 
     for subj in range(n_subs):
-        # --- MAX sui lag per questo soggetto ---
-        subj_strength_max = np.full((n_ch, n_ch), 0.0)
+        # mean on lags 
+        subj_strength_sum = np.full((n_ch, n_ch), 0.0)
+        subj_n_probs_sum = np.full((n_ch, n_ch), 0.0)
         
         for lag in results.keys():  # 0,...,maxlag
-            s = np.abs(results[lag][group]["strengths"][subj])
+            s = results[lag][group]["strengths"][subj]
             p = results[lag][group]["probs"][subj]
 
-            mask = (p >= min_prob)
-            s_filt = np.where(mask, s, 0.0)
+            mask = (p >= min_prob) # keep only strengths w higher probabilities
+            s_masked = np.where(mask, s, 0.0)
 
-            subj_strength_max = np.maximum(subj_strength_max, s_filt)
-
-    #     # --- MAX sui soggetti ---
-    #     group_strength_max = np.maximum(group_strength_max, subj_strength_max)
-    # res_groups[group]["strength"] = group_strength_max
+            subj_strength_sum += s_masked
+            subj_n_probs_sum += mask.astype(int) # 1->accepted, 0->not accepted
         
-        # --- MEAN sui soggetti ---
-        group_strength_sum += subj_strength_max
-    res_groups[group]["strength"] = group_strength_sum / n_subs    
+        # mean strength and accepted probs for this subject
+        subj_strength_mean = subj_strength_sum / n_lags
+        subj_n_probs_mean = subj_n_probs_sum / n_lags
 
+        group_strength_sum += subj_strength_mean
+        group_n_probs_sum += subj_n_probs_mean
 
-#****no lags aggragation
-# for lag,all_groups in results.items():
-#     for group,all_ecns in all_groups.items():
-#         strength_group_tot = np.zeros((n_ch, n_ch))
-#         prob_group_tot = np.zeros((n_ch, n_ch))
+    # mean for group
+    res_groups[group]["strength"] = group_strength_sum / n_subs
+    res_groups[group]["n_probs"] = group_n_probs_sum / n_subs
 
-#         # keep highest causal links (i.e. the most certain ones -> prob>=min_prob)
-#         n_subs = len(all_ecns["strengths"])
-#         for subj in range(0,n_subs):
-#             s = all_ecns["strengths"][subj]
-#             p = all_ecns["probs"][subj]
-#             prob_bin = (p >= min_prob).astype(int) # binary (1->link; 0->no link)
-#             strength_filtered = s*prob_bin # keep most probable strengths
-
-#             # accumulate for mean
-#             strength_group_tot += strength_filtered
-#             prob_group_tot += prob_bin
-#         # mean across subs
-#         mean_strength = strength_group_tot / n_subs       
-#         # accumulate mean lag contribution for group
-#         lag_contribute = mean_strength / n_tau
-#         res_groups[group]["strength"] += lag_contribute
-
-#         # accumulate number of accepted probs for group  
-#         res_groups[group]["n_probs"] += prob_group_tot / n_subs
-
-# find global threhsold and min and max for min-max norm [0,1] for pretty plots
-all_strengths = []
-max_s, min_s = 0, math.inf
+# for pretty plots
+all_strengths, all_n_probs = [], []
+max_n_pr, min_n_pr = 0, math.inf # min and max for min-max norm [0,1]
 for group, ecn in res_groups.items(): 
     s = np.abs(ecn["strength"])
-    all_strengths.append(s.ravel()) # from 2D to 1D
+    all_strengths.append(s.ravel()) # 2D->1D
 
-    #if np.min(s) < min_s: min_s = np.min(s)
-    if np.max(s) > max_s: max_s = np.max(s)
+    p = ecn["n_probs"]
+    if np.min(p) < min_n_pr: min_n_pr = np.min(p)
+    if np.max(p) > max_n_pr: max_n_pr = np.max(p)
 all_strengths = np.concatenate(all_strengths)
-min_thresh = np.percentile(all_strengths, thresh_pct) # theshold!
+min_thresh = np.percentile(all_strengths, thresh_pct) # global threshold for strengths!
 
 
 # plot results for each group
 fig, axes = plt.subplots(1, 3, figsize=(13, 6), constrained_layout=True) 
 for group, ecn in res_groups.items():
         s = np.abs(ecn["strength"])
-        #p = ecn["n_probs"]
-        s_norm = (s-min_thresh) / (max_s-min_thresh) # min-max norm for values>=min_threshold [0,1]
+        
+        p = ecn["n_probs"]
+        p_norm = (p-min_n_pr) / (max_n_pr-min_n_pr) # min-max norm [0,1]
 
         strength_group_df = pd.DataFrame(s, index=ch_names, columns=ch_names)
-        norm_df = pd.DataFrame(1.5*s_norm, index=ch_names, columns=ch_names)
-        #n_probs_group_df = pd.DataFrame(p, index=ch_names, columns=ch_names)
+        p_norm_group_df = pd.DataFrame(p_norm, index=ch_names, columns=ch_names)
         
-        plot_ecn(strength_group_df, min_thresh, ax=axes[pos[group]], title=group, widths=norm_df)
+        plot_ecn(strength_group_df, min_thresh, ax=axes[pos[group]], title=group, widths=p_norm_group_df)
 
 fig.suptitle(f"LiNGAM (prob_min={min_prob}, th_pct={thresh_pct})", fontsize=16)
 #plt.tight_layout() # useless if constrained_layout=True
 plt.show()
 
 save_results() # save figures
-
-# fig, axes = plt.subplots(1, 3, figsize=(13, 6), constrained_layout=True)
-# for group, all_ecns in results.items():
-#     strength_group = np.zeros_like(results[group]["strengths"][0])
-
-#     # keep highest causal links
-#     for subj in range(0,len(all_ecns["strengths"])):
-#         strength_group = np.maximum(
-#             strength_group, np.abs(all_ecns["strengths"][subj]))
-        
-#     strength_group_df = pd.DataFrame(
-#             strength_group, index=ch_names, columns=ch_names)
-    
-#     plot_ecn(strength_group_df, thresh, ax=axes[pos[group]], title=group)
-
-# fig.suptitle(f"LiNGAM (highest vals, th={thresh})", fontsize=16)
-# #plt.tight_layout() # useless if constrained_layout=True
-# plt.show()
-
-# save_results() # save figures
