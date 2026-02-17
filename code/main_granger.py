fe1984dc2f259fbc7cf88649636c9c0d3996b712
Lookediddle <luca.giussani2@studenti.unimi.it>
2026-02-17 17:34:04 +0100

    final code modifications before deposition

--- code/main_granger.py
+++ code/main_granger.py
@@ -18,42 +18,42 @@ subs_to_groups = {num:label for r,label in ranges for num in r} # e.g. {1:"AD", 
 
 #%% granger preprocessing: check stationarity
 print('--- check stationarity ---')
-# all_subs_report = {}
-# for subj_dir in subjects:
-#     print(f"\n- {subj_dir.name} -", end=' ', flush=True)
-#     report = analyze_subject_stationarity(subj_dir, resample=fs_res, n_epochs=n_epochs)
-#     all_subs_report[subj_dir.name] = report
-# save_results(all_subs_report)
-all_subs_report = load_data("results/20260128_110832_allsubs_stationarity/data/saved_data.pkl")
+all_subs_report = {}
+for subj_dir in subjects:
+    print(f"\n- {subj_dir.name} -", end=' ', flush=True)
+    report = analyze_subject_stationarity(subj_dir, resample=fs_res, n_epochs=n_epochs)
+    all_subs_report[subj_dir.name] = report
+save_results(all_subs_report)
+#all_subs_report = load_data("results/20260128_110832_allsubs_stationarity/data/saved_data.pkl") # use saved data for speed!
 
 #%% process ECN
 print('--- Granger causality ---')
-# results = {}
-# for lag in range(1,maxlag+1):
-#     results[lag] = {
-#         "AD":  {"pvals": []},
-#         "FTD": {"pvals": []},
-#         "CN":  {"pvals": []}}
+results = {}
+for lag in range(1,maxlag+1):
+    results[lag] = {
+        "AD":  {"pvals": []},
+        "FTD": {"pvals": []},
+        "CN":  {"pvals": []}}
 
-# for subj_dir in subjects:
-#     subj_id = subj_dir.name # i.e. 'sub-xxx'
-#     subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. subs_to_groups[int('xxx')]
-#     print(f"\n- {subj_id} -", end=' ', flush=True)
+for subj_dir in subjects:
+    subj_id = subj_dir.name # i.e. 'sub-xxx'
+    subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. subs_to_groups[int('xxx')]
+    print(f"\n- {subj_id} -", end=' ', flush=True)
 
-#     #%% preprocessing: load and segment
-#     filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
-#     eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
-#     epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
+    #%% preprocessing: load and segment
+    filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
+    eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
+    epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
     
-#     #%% granger 
-#     curr_sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
-#     gran_pvals = granger_ecn(epochs, channels, maxlag, curr_sub)
+    #%% granger 
+    curr_sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
+    gran_pvals = granger_ecn(epochs, channels, maxlag, curr_sub)
     
-#     for lag in range(1,maxlag+1):
-#         results[lag][subj_group]["pvals"].append(gran_pvals[lag])
+    for lag in range(1,maxlag+1):
+        results[lag][subj_group]["pvals"].append(gran_pvals[lag])
 
-# save_results(results)
-results = load_data("results/20260215_154921_gran_plots_th80_alpha0_05_agg1/data/saved_data.pkl")
+save_results(results)
+#results = load_data("results/20260215_154921_gran_plots_th80_alpha0_05_agg1/data/saved_data.pkl") # use saved data for speed!
 
 #%% aggregate ECNs for each group
 max_pval = alpha # if close to 0, only most certain causal links are considered
--- code/main_lingam.py
+++ code/main_lingam.py
@@ -19,44 +19,44 @@ subs_to_groups = {num:label for r,label in ranges for num in r} # e.g. {1:"AD", 
 
 #%% VAR(-lingam) preprocessing: check stationarity
 print('--- check stationarity ---')
-# all_subs_report = {}
-# for subj_dir in subjects:
-#     print(f"\n- {subj_dir.name} -", end=' ', flush=True)
-#     report = analyze_subject_stationarity(subj_dir, resample=fs_res, n_epochs=n_epochs)
-#     all_subs_report[subj_dir.name] = report
-# save_results(all_subs_report)
-all_subs_report = load_data("results/20260128_110832_allsubs_stationarity/data/saved_data.pkl")
+all_subs_report = {}
+for subj_dir in subjects:
+    print(f"\n- {subj_dir.name} -", end=' ', flush=True)
+    report = analyze_subject_stationarity(subj_dir, resample=fs_res, n_epochs=n_epochs)
+    all_subs_report[subj_dir.name] = report
+save_results(all_subs_report)
+#all_subs_report = load_data("results/20260128_110832_allsubs_stationarity/data/saved_data.pkl") # use saved results for speed!
 
 #%% process ECN
 print('--- VAR-LiNGAM causality ---')
-# results = {}
-# for lag in range(maxlag+1): # 0,...,maxlag
-#     results[lag] = {
-#         "AD":  {"strengths": [], "probs": []},
-#         "FTD": {"strengths": [], "probs": []},
-#         "CN":  {"strengths": [], "probs": []}}
+results = {}
+for lag in range(maxlag+1): # 0,...,maxlag
+    results[lag] = {
+        "AD":  {"strengths": [], "probs": []},
+        "FTD": {"strengths": [], "probs": []},
+        "CN":  {"strengths": [], "probs": []}}
 
-# for subj_dir in subjects:
-#     subj_id = subj_dir.name # i.e. 'sub-xxx'
-#     subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. int('xxx')
-#     print(f"\n- {subj_id} -", end=' ', flush=True)
+for subj_dir in subjects:
+    subj_id = subj_dir.name # i.e. 'sub-xxx'
+    subj_group = subs_to_groups[int(subj_id[-3:])] # i.e. int('xxx')
+    print(f"\n- {subj_id} -", end=' ', flush=True)
 
-#     #%% preprocessing: load and segment
-#     filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
-#     eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
-#     epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
+    #%% preprocessing: load and segment
+    filepath = list((subj_dir / "eeg").glob("*_eeg.set"))[0]
+    eeg, _, channels = load_eeg(filepath, resample=fs_res, preload=True) # notice resampling!
+    epochs = split_epochs(eeg, n_epochs=n_epochs) # split into 10 equal segments
         
-#     #%% lingam
-#     sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
-#     # lingam+bootstrap
-#     ling_strength, ling_probs = lingam_ecn_boot(epochs, channels, maxlag, current_subject=sub)
+    #%% lingam
+    sub = all_subs_report[subj_id] # to skip unnecessary stationarity checks
+    # lingam+bootstrap
+    ling_strength, ling_probs = lingam_ecn_boot(epochs, channels, maxlag, current_subject=sub)
 
-#     for lag in range(maxlag+1): # 0,...,maxlag
-#         results[lag][subj_group]["strengths"].append(ling_strength[lag])
-#         results[lag][subj_group]["probs"].append(ling_probs[lag])
+    for lag in range(maxlag+1): # 0,...,maxlag
+        results[lag][subj_group]["strengths"].append(ling_strength[lag])
+        results[lag][subj_group]["probs"].append(ling_probs[lag])
 
-# save_results(results)
-results = load_data("results/20260215_154600_ling-bootstrap_minefx0_2_plots_th80_prob95_agg1/data/saved_data.pkl")
+save_results(results)
+#results = load_data("results/20260215_154600_ling-bootstrap_minefx0_2_plots_th80_prob95_agg1/data/saved_data.pkl") # use saved results for speed
 
 #%% aggregate ECNs for each group
 min_prob = 0.95 # ~alpha=0.05
@@ -65,7 +65,7 @@ res_groups = aggregate_lingam(results, min_prob) # strengths and probabilities
 #%% plot ECNs for each group
 ch_names = results[0]["AD"]["strengths"][0].columns # remind indexes' names = columns' names
 pos = {"CN":0,"FTD":1,"AD":2}
-thresh_pct=85 # strengths threshold (percentile)
+thresh_pct=80 # strengths threshold (percentile)
 
 all_strengths, all_n_probs = [], []
 for group, ecn in res_groups.items(): 
